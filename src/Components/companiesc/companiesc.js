a03f49d61609c7107fb5c2e018c36011ca416c2f
Anupam Dey <anupamdey2001@gmail.com>
2024-01-18 11:28:08 +0530

    first commit

---
+++ src/Components/companiesc/companiesc.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react";
+import NavBar from "../NavBar";
+import Navbarm from "../NavbarM";
+import Footer from "../Footer";
+function Companies() {
+    return(<div>
+        <span className="rcr">COMPANIES RECRUITING</span>
+        <Navbarm/>   
+      <Footer/>
+    </div>)
+}
+
+export default Companies;
